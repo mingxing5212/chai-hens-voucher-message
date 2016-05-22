0c7e2a194a77d4750884ae33ed23409068e22320
mingxing.li <mingxing.li@ele.me>
2016-05-22 14:50:42 +0800

    remove jar from target

--- chai-hens-voucher-message-app/src/main/java/me/mingxing5212/chaihens/voucher/Application.java
+++ chai-hens-voucher-message-app/src/main/java/me/mingxing5212/chaihens/voucher/Application.java
@@ -1,11 +1,9 @@
 package me.mingxing5212.chaihens.voucher;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.velocity.VelocityAutoConfiguration;
-import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication 
 @EnableAutoConfiguration(exclude = { VelocityAutoConfiguration.class })
